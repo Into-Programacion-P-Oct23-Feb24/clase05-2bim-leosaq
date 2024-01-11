b525b25b8ec9d796749ac79a4660399e650f74ab
leosaq <leandrosaquisari7@gmail.com>
2024-01-11 17:24:27 -0500

    Cambios

--- EjemplosRecursividad/src/Paquete03/Clase01.java
+++ EjemplosRecursividad/src/Paquete03/Clase01.java
@@ -9,5 +9,13 @@ package Paquete03;
  * @author leand
  */
 public class Clase01 {
-    
+    public static void main (String[] args){
+        
+    }
+    public static String[][] obtenerCadena (){
+        String [][] cadena = {{"Loja","Quito","Cuenca"},
+            {"Esmeraldas","Guayaquil","Ibarra"}};
+        return cadena;
+        
+    }
 }
